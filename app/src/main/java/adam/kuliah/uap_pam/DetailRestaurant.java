c5bdb014495dc3a584c0e70e170827b038846512
adamdaffa56 <89517434+adamdaffa56@users.noreply.github.com>
2023-06-21 18:36:56 +0700

    Update DetailRestaurant.java

--- app/src/main/java/adam/kuliah/uap_pam/DetailRestaurant.java
+++ app/src/main/java/adam/kuliah/uap_pam/DetailRestaurant.java
@@ -1,23 +1,35 @@
 package adam.kuliah.uap_pam;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
 
 public class DetailRestaurant extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tvName, tvAddress, tvBusinessHour, tvDescription;
     private ImageView ivRestaurant, btn_back;
-    private Button btnDetailEdit, btnDetailHapus;
+
+    private String restaurantName, imgUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +70,54 @@ public class DetailRestaurant extends AppCompatActivity implements View.OnClickL
                     .load(imageURL)
                     .apply(requestOptions)
                     .into(ivRestaurant);
+        }
+    }
+
+    // Download image
+    private void downloadImage() {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+
+        try {
+            // Create storage reference
+            StorageReference storageRef = storage.getReferenceFromUrl(imgUrl);
+
+            File localFile;
+            File storagePath = new File(getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "");
+
+            if (!storagePath.exists()) {
+                storagePath.mkdirs();
+            }
+
+            if(storagePath.exists()) {
+                localFile = storagePath;
+            } else {
+                localFile = getApplication().getExternalFilesDir("images");
+            }
+
+            final File myFile = new File(localFile, restaurantName + ".jpg");
+
+            storageRef.getFile(myFile).addOnSuccessListener(taskSnapshot -> {
+                // Local file berhasil created
+                Toast.makeText(this, "File downloaded", LENGTH_SHORT).show();
+            }).addOnFailureListener(exception -> {
+                // Handle errors
+                Toast.makeText(getBaseContext(), "Download failed. Try again!", LENGTH_SHORT).show();
+                exception.printStackTrace();
+            });
+
+
+            new Handler().postDelayed(() -> {
+                ContentValues values = new ContentValues();
+
+                values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis());
+                values.put(MediaStore.Images.Media.DISPLAY_NAME, restaurantName);
+                values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+                values.put(MediaStore.MediaColumns.DATA, myFile.getAbsolutePath());
+                getApplicationContext().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            }, 1000);
+            ContentValues values = new ContentValues();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
